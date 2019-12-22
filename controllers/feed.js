c2856217f1ee42efa62d72f893a794e4cf9ddbd8
Eran Krakovsky <ekrako@gmail.com>
2019-12-22 22:41:45 +0200

    add post validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,41 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, _next) => {
   return res.status(200).json({
-    posts: [{ title: 'first post', content: 'hello world!' }]
+    posts: [{
+      _id: '1',
+      title: 'first post',
+      content: 'hello world!',
+      creator: {
+        name: 'Eran Krakovsky'
+      },
+      createdAt: Date.now(),
+      imageUrl: 'images/ducky.jpg'
+    }], totalPosts: 1
   });
 };
 
 exports.createPost = (req, res, _next) => {
   const title = req.body.title;
   const content = req.body.content;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'bad input, enter valid input',
+      errors: errors.array()
+    });
+  }
   return res.status(201).json({
     message: 'Post created successfully',
-    post: { id: new Date().toISOString(), title, content }
+    post: {
+      _id: '1',
+      title,
+      content,
+      creator: {
+        name: 'Eran Krakovsky'
+      },
+      createdAt: Date.now(),
+      imageUrl: 'images/ducky.jpg'
+    }
   });
 };
