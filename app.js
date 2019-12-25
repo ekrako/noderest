96670c0b20d6500f132298f9533f43eba1e40a76
Eran Krakovsky <ekrako@gmail.com>
2019-12-25 15:37:25 +0200

    added pagination

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const secrets = require('./config/secret');
 const path = require('path');
 
-// const cors = require('cors');
+const cors = require('cors');
 
 const feedRoutes = require('./routes/feed');
 const errorRoutes = require('./controllers/error');
 const multer = require('multer');
-
+const uuidv4 = require('uuid/v4');
 
 const app = express();
 
@@ -18,27 +18,31 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images')
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, file.filename + '-' + Date.now() + '-' + file.originalname)
+    cb(null, uuidv4() + file.originalname);
   }
-})
+});
 const fileFilter = (req, file, cb) => {
-  cb(null, file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg')
-}
+  cb(
+    null,
+    file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+  );
+};
 
 app.use(multer({ storage, fileFilter }).single('image'));
 
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  next();
-});
+//   next();
+// });
 app.use('/feed', feedRoutes);
 app.use(errorRoutes.error404);
 app.use(errorRoutes.errorHandler);
@@ -46,7 +50,8 @@ mongoose
   .connect(secrets.mongoConnectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }).then(_result => {
+  })
+  .then(_result => {
     app.listen('8080', () => {
       console.log('server listen http://localhost:8080');
     });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,35 +1,45 @@
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 const fileHelper = require('../util/file');
-
+const ITEMS_PER_PAGE = 2;
 
 exports.getPosts = (req, res, next) => {
-  Post.find().then(posts => {
-    return res.status(200).json({
-      posts,
-      totalPosts: posts.length
+  const page = +req.query.page || 1;
+  let totalItems;
+  Post.countDocuments()
+    .then(total => {
+      totalItems = total;
+      return Post.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then(posts => {
+      return res.status(200).json({
+        posts,
+        totalItems
+      });
+    })
+    .catch(err => {
+      next(err);
     });
-  }).catch(err => {
-    next(err);
-  });
-
 };
 
 exports.getPost = (req, res, next) => {
   const postId = req.params.postId;
-  Post.findById(postId).then(post => {
-    if (!post) {
-      const error = new Error('No post was found');
-      error.statusCode = 404;
-      throw error;
-    }
-    return res.status(200).json({
-      post
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('No post was found');
+        error.statusCode = 404;
+        throw error;
+      }
+      return res.status(200).json({
+        post
+      });
+    })
+    .catch(err => {
+      next(err);
     });
-  }).catch(err => {
-    next(err);
-  });
-
 };
 
 exports.createPost = (req, res, next) => {
@@ -47,20 +57,22 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  const imageUrl = image.path;
+  const imageUrl = image.path.replace(/\\/g, '/');
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: { name: 'Eran' },
+    creator: { name: 'Eran' }
   });
-  post.save()
+  post
+    .save()
     .then(result => {
       return res.status(201).json({
         message: 'Post created successfully',
         post: result
-      })
-    }).catch(err => {
+      });
+    })
+    .catch(err => {
       next(err);
     });
 };
@@ -73,47 +85,54 @@ exports.editPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  Post.findById(postId).then(post => {
-    if (!post) {
-      const error = new Error('No post was found');
-      error.statusCode = 404;
-      throw error;
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-    const imageUrl = req.file ? req.file.path : req.body.image;
-    const image = req.file;
-    if (image) {
-      fileHelper.deleteFile(post.imageUrl);
-    }
-    post.imageUrl = imageUrl;
-    post.title = title;
-    post.content = content;
-    return post.save();
-  }).then(post => {
-    return res.status(200).json({
-      post
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('No post was found');
+        error.statusCode = 404;
+        throw error;
+      }
+      const title = req.body.title;
+      const content = req.body.content;
+      const imageUrl = req.file
+        ? req.file.path.replace(/\\/g, '/')
+        : req.body.image;
+      const image = req.file;
+      if (image) {
+        fileHelper.deleteFile(post.imageUrl);
+      }
+      post.imageUrl = imageUrl;
+      post.title = title;
+      post.content = content;
+      return post.save();
+    })
+    .then(post => {
+      return res.status(200).json({
+        post
+      });
+    })
+    .catch(err => {
+      next(err);
     });
-  }).catch(err => {
-    next(err);
-  });
 };
 
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
-  Post.findById(postId).then(post => {
-    if (!post) {
-      const error = new Error('No post was found');
-      error.statusCode = 404;
-      throw error;
-    }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('No post was found');
+        error.statusCode = 404;
+        throw error;
+      }
 
-    fileHelper.deleteFile(post.imageUrl);
-    return Post.findByIdAndRemove(postId)
-  }).then(() => {
-    return res.status(200).json({ message: 'post deletes' })
-  }).catch(err => {
-    next(err);
-  });
-
+      fileHelper.deleteFile(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(() => {
+      return res.status(200).json({ message: 'post deletes' });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
