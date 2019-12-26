814341ccdfa339192bc07175f73bd8d00589e579
Eran Krakovsky <ekrako@gmail.com>
2019-12-26 16:49:55 +0200

    changed to aysnc await

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,93 +4,87 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const secrets = require('../config/secret');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validateion failed, incorrect Data was enetered');
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-  const email = req.body.email;
-  const name = req.body.name;
-  const password = req.body.password;
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        const error = new Error('User already exist  found');
-        error.statusCode = 422;
-        error.data = errors.array();
-      }
-      return bcrypt.hash(password, 12);
-    })
-    .then(hashedPassword => {
-      const user = new User({
-        email,
-        name,
-        password: hashedPassword,
-        posts: []
-      });
-      return user.save();
-    })
-    .then(result => {
-      return res.status(201).json({
-        userId: result._id,
-        message: 'user Created'
-      });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      next(error);
+  try {
+    if (!errors.isEmpty()) {
+      const error = new Error(
+        'Validateion failed, incorrect Data was enetered'
+      );
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      const error = new Error('User already exist  found');
+      error.statusCode = 422;
+      error.data = errors.array();
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      email,
+      name,
+      password: hashedPassword,
+      posts: []
     });
+    await newUser.save();
+    return res.status(201).json({
+      userId: newUser._id,
+      message: 'user Created'
+    });
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    next(error);
+  }
 };
 
-exports.login = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validateion failed, incorrect Data was enetered');
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-  let loginUser;
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        const error = new Error('Wrong email and password combination');
-        error.statusCode = 401;
-      }
-      loginUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-
-    .then(isAuth => {
-      if (!isAuth) {
-        const error = new Error('Wrong email and password combination');
-        error.statusCode = 401;
-      }
-      const token = jwt.sign(
-        {
-          email: loginUser.email,
-          userId: loginUser._id,
-          name: loginUser.name
-        },
-        secrets.jwtSecret,
-        { expiresIn: '1h' }
+exports.login = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error(
+        'Validateion failed, incorrect Data was enetered'
       );
-      return res.status(200).json({
-        userId: loginUser._id.toString(),
-        status: loginUser.status,
-        token,
-        message: 'user Login successfully'
-      });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      next(error);
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error('Wrong email and password combination');
+      error.statusCode = 401;
+      throw error;
+    }
+    const isAuth = await bcrypt.compare(password, user.password);
+    if (!isAuth) {
+      const error = new Error('Wrong email and password combination');
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id,
+        name: user.name
+      },
+      secrets.jwtSecret,
+      { expiresIn: '1h' }
+    );
+    return res.status(200).json({
+      userId: user._id.toString(),
+      status: user.status,
+      token,
+      message: 'user Login successfully'
     });
+  } catch (err) {
+    next(err);
+  }
 };
--- controllers/status.js
+++ controllers/status.js
@@ -2,45 +2,41 @@ const User = require('../models/user');
 
 const { validationResult } = require('express-validator');
 
-exports.getStatus = (req, res, next) => {
+exports.getStatus = async (req, res, next) => {
   const userId = req.userId;
-  User.findById(userId)
-    .then(user => {
-      if (!user) {
-        const error = new Error('User was not found');
-        error.statusCode = 422;
-        throw error;
-      }
-      return res.status(200).json({ status: user.status });
-    })
-    .catch(err => {
-      next(err);
-    });
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      const error = new Error('User was not found');
+      error.statusCode = 422;
+      throw error;
+    }
+    return res.status(200).json({ status: user.status });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.setStatus = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Status cannot be empty');
-    error.statusCode = 422;
-    throw error;
+exports.setStatus = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Status cannot be empty');
+      error.statusCode = 422;
+      throw error;
+    }
+    const userId = req.userId;
+    const status = req.body.status;
+    const user = await User.findById(userId);
+    if (!user) {
+      const error = new Error('User was not found');
+      error.statusCode = 422;
+      throw error;
+    }
+    user.status = status;
+    await user.save();
+    return res.status(200).json({ status: user.status });
+  } catch (err) {
+    next(err);
   }
-  const userId = req.userId;
-  const status = req.body.status;
-  User.findById(userId)
-    .then(user => {
-      if (!user) {
-        const error = new Error('User was not found');
-        error.statusCode = 422;
-        throw error;
-      }
-      user.status = status;
-      return user.save();
-    })
-    .then(user => {
-      return res.status(200).json({ status: user.status });
-    })
-    .catch(err => {
-      next(err);
-    });
 };
